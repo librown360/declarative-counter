2f4648aab839603538d89751e28126f9951d26f5
Lisa Brown <lisaann493@verizon.net>
2022-03-08 21:44:34 -0500

    Declarative rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// Imports
+import React from "react";
+import { useState } from "react";
 
+// Function to render a simple counter
 function App() {
+  let [ counter, changeCounter ] = useState(1)
+  // making the update function global to modify in the console
+  window.changeCounter = changeCounter
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* use the counter in multiple tags */}
+      <h1>This is my big header counter: {counter}</h1>
+      <h4>This is my little header counter: {counter}</h4>
     </div>
-  );
+    
+
+  )
 }
 
 export default App;
